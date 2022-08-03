78af5e81e120de80375c26847ac610d9e8159bca
Nii <bnstephens001@gmail.com>
2022-08-03 14:26:58 +0000

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,32 @@
+import React, {useEffect, useState} from "react"
+
+
 // create your App component here
+// Employ the useEFfect Hook
+// send a fetch request inside the useEffect hook with the URL
+// Display a <p> tag with the text loading when text is rendered the first time
+//After API is rendered show dog image with the img tag and alt attribute "A random Dog"
+//Export App 
+
+function App () {
+    const [image, setImage] = useState("")
+
+    
+    useEffect (() => {
+        fetch ("https://dog.ceo/api/breeds/image/random")
+        .then ((r) => r.json())
+        .then ((data) => setImage(data.message))
+    }, [])
+
+    //if the page is yet to render anything show loading in <p> tag.
+    if(!image) return <p>Loading...</p>
+
+    return (
+        <div>
+            <img src = {image} alt = "A Random Dog" />
+        </div>
+    )
+
+}
+
+export default App;
